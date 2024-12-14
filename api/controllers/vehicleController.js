5d1464abc10f2cb491d8ffde7b0ccc64af1cb1a8
Nahom Tsegaye <tnahom214@gmail.com>
2024-12-14 16:50:39 +0300

    backend fixed

--- api/controllers/vehicleController.js
+++ api/controllers/vehicleController.js
@@ -1,25 +1,24 @@
-import VehicleModel from "../models/Vehicles.js";
-
+import VehicleModel from "../models/Vehicles.js"
 
 export const createVehicle = async (req, res) => {
-  try {
-    const vehicle = await VehicleModel.create(req.body);
-    res.status(201).json(vehicle);
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
-};
+    try {
+      const vehicle = await VehicleModel.create(req.body);
+      res.status(201).json(vehicle);
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  };
 
-export const getVehicles = async (req, res) => {
-  try {
-    const vehicles = await VehicleModel.find({});
-    res.status(200).json(vehicles);
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
-};
-
-export const getVehicleById = async (req, res) => {
+  export const getVehicles = async (req, res) => {
+    try {
+      const vehicles = await VehicleModel.find({});
+      res.status(200).json(vehicles);
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  };
+  
+  export const getVehicleById = async (req, res) => {
     try {
         const { id } = req.params;
         const vehicle = await VehicleModel.findById(id);
@@ -32,7 +31,7 @@ export const getVehicleById = async (req, res) => {
     }
 };
 
-export const updateVehicle = async (req, res) => {
+  export const updateVehicle = async (req, res) => {
     try {
         const { id } = req.params;
         const updatedVehicle = await VehicleModel.findByIdAndUpdate(id, req.body, { new: true });
